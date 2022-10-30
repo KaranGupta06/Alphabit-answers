56c4fe1527edb471e8e4e3143a6479af989a1dae
Karan Gupta <80627097+KaranGupta06@users.noreply.github.com>
2022-10-30 21:17:16 +0530

    Update PRE minesweeper.py

--- PRE minesweeper.py
+++ PRE minesweeper.py
@@ -1,16 +1,26 @@
 l, b = map(int, input().split())
+n = int(input())
 matrix = []
-count = 0
 
 for _ in range(b):
     matrix.append(list(map(int, input().split())))
 
+for i in range(b):
+    for j in range(l):
+        if matrix[i][j] == 1:
+            matrix[i][j] = 'm'
+
 for i in range(l):
     for j in range(b):
-        if matrix[j][i] == 1:
+        if matrix[j][i] == 'm':
             for g in range(9):
                 x_d, y_d = g//3-1, g%3-1
-                if j + y_d in range(b) and i + x_d in range(l) and matrix[j+y_d][i+x_d] == 0:
-                    count += 1
+                if j + y_d in range(b) and i + x_d in range(l) and matrix[j+y_d][i+x_d] != 'm':
+                    matrix[j+y_d][i+x_d] += 1
 
+count = 0
+for i in matrix[n]:
+    if i != 'm':
+        count += i
+    
 print(count)
